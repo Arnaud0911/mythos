05562813d98070214909923f4ed97182e2190d8e
Arnaud0911 <63853018+Arnaud0911@users.noreply.github.com>
2026-08-01 10:38:01 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION='6.0.0';
+const VERSION='6.1.0';
 const SHELL=`mythos-shell-${VERSION}`;
 const RUNTIME=`mythos-runtime-${VERSION}`;
 const SHELL_FILES=['./','./index.html','./manifest.webmanifest','./version.json','./icons/favicon.png','./icons/apple-touch-icon.png','./icons/icon-192.png','./icons/icon-512.png'];
